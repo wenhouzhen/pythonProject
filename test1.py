d75a07ed5b5f88b8c73e4107a5208e0ee3c25848
温后珍 <wenhouzhen@163.com>
2024-05-01 13:57:30 +0800

    试五试

--- test1.py
+++ test1.py
@@ -35,7 +35,7 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "关闭"))
+        self.pushButton.setText(_translate("Dialog", "关闭逼"))
 
 
 if __name__ == '__main__':
